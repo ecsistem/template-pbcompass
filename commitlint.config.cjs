2e3871037e375b21fc0760bb2278f88d0f2fe942
Edson Costa <edson.13.10.2004@gmail.com>
2023-09-12 13:59:17 -0300

    chore: add commitlint e husky

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,21 +1,4 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    rules: {
-      'type-enum': [
-        2,
-        'always',
-        [
-          // Lista de tipos de commit permitidos
-          'feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci',
-  
-          // Permitir um tipo "aleatório" (qualquer coisa que siga o formato prefixo: mensagem)
-          /^(.+: .+)$/,
-        ],
-      ],
-      'type-empty': [0, 'never'], // Impedir tipos de commit vazios
-      'subject-empty': [0, 'never'], // Permitir mensagens de commit vazias
-      // 'subject-full-stop': [2, 'always', '.'], // Certifique-se de que a mensagem termine com um ponto.
-      // 'subject-case': [2, 'always', 'sentence-case'], // Certifique-se de que a mensagem esteja em "sentence case".
-    },
-  };
+    };
   
