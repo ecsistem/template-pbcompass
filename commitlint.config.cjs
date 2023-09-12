cd29ec10eeeb4f0e4a24ce2e3633d25b5cec9152
Edson Costa <edson.13.10.2004@gmail.com>
2023-09-12 00:05:08 -0300

    chore: lint on commitmsg

---
+++ commitlint.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        [
+          // Lista de tipos de commit permitidos
+          'feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci',
+  
+          // Permitir um tipo "aleatório" (qualquer coisa que siga o formato prefixo: mensagem)
+          /^(.+: .+)$/,
+        ],
+      ],
+      // 'subject-full-stop': [2, 'always', '.'], // Certifique-se de que a mensagem termine com um ponto.
+      'subject-case': [2, 'always', 'sentence-case'], // Certifique-se de que a mensagem esteja em "sentence case".
+    },
+  };
+  
